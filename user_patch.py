1bf19ad258e7d21a03d81da8df43a3969c4db7e5
proprefenetre <proprefenetre@gmail.com>
2019-07-15 12:57:23 +0200

    refactoring

--- user_patch.py
+++ user_patch.py
@@ -1,14 +1,19 @@
 import json
 import re
 
-with open('config.py', 'r') as fh:
-	config = fh.read()
+with open("config.py", "r") as fh:
+    config = fh.read()
 
-usr_json = json.load(open('cfg/users.json', 'r'))
+with open("users.json", "r") as f:
+    usr_json = json.load(f)
 
-usr_str = '%s,' % ',\n'.join(["'{}': '{}'".format(u, p) for u, p in usr_json.items()])
+users = ",\n".join([f"'{{{u}}}': '{{{p}}}'" for u, p in usr_json.items()])
 
-new_config = re.sub('USER_PASSWORD = \{([^}]+)\}', 'USER_PASSWORD = {\g<1>%s\n}' % usr_str, config)
+usr_str = f"{users}"
 
-with open ('config.py', 'w') as fh:
-	fh.write(new_config)
+new_config = re.sub(r"USER_PASSWORD = \{([^}]+)\}",
+                    r"USER_PASSWORD = {\g<1>%s\n}" % usr_str,
+                    config)
+
+with open('config.py', 'w') as fh:
+    fh.write(new_config)
